0d68fb457c354b05803c783394f60369d8b92f0c
Robin Allen <114621789+robinallenaz@users.noreply.github.com>
2024-11-03 14:55:56 -0700

    Created mongoose.js

---
+++ db/mongoose.js
@@ -0,0 +1,12 @@
+
+import mongoose from 'mongoose';
+async function connect() {
+  try {
+    await mongoose.connect('mongodb+srv://admin:admin@cluster0.cgnmk.mongodb.net/test');
+    console.log('Connected to MongoDB');
+  } catch (err) {
+    console.error(err);
+  }
+}
+
+export default connect;
