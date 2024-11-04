23215c47f477eaa7dbd4c27008d2c79fecfded84
Robin Allen <114621789+robinallenaz@users.noreply.github.com>
2024-11-03 18:22:09 -0700

    Fixed Mongoose model definition

--- db/conn.mjs
+++ db/conn.mjs
@@ -1,28 +1,36 @@
 //Importing mongoose
 import mongoose from "mongoose";
+// Mongoose model definition
+const gradeSchema = new mongoose.Schema({
+  learner_id: Number,
+  class_id: Number,
+  scores: [
+    {
+      type: { type: String, enum: ['exam', 'homework', 'quiz'] },
+      score: Number,
+    },
+  ],
+});
+
+// Create the model
+const Grade = mongoose.model("Grade", gradeSchema);
+
+// Export the model
+export default Grade;
 
 // import { MongoClient } from "mongodb";
 
-// Load environment variables from .env
-import dotenv from "dotenv";
-dotenv.config();
 
-const gradesSchema = new mongoose.Schema({
-  // Schema fields
-});
+// async function connect() {
+//   try {
+//     await mongoose.connect(process.env.ATLAS_URI);
+//     console.log('Connected to MongoDB');
+//   } catch (err) {
+//     console.error(err);
+//   }
+// }
 
-const Grades = mongoose.model('Grades', gradesSchema);
-
-async function connect() {
-  try {
-    await mongoose.connect(process.env.ATLAS_URI);
-    console.log('Connected to MongoDB');
-  } catch (err) {
-    console.error(err);
-  }
-}
-
-export default { Grades, connect };
+// export default { Grades, connect };
 
 // const client = new MongoClient(process.env.ATLAS_URI);
 
