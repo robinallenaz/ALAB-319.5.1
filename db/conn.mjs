a55ef1ad6eef3316bd592c19fa1edd3b32ddac55
Robin Allen <114621789+robinallenaz@users.noreply.github.com>
2024-11-03 17:40:30 -0700

    Updated the conn.mjs file to use Mongoose instead of the MongoDB driver

--- db/conn.mjs
+++ db/conn.mjs
@@ -1,26 +1,46 @@
-import { MongoClient } from "mongodb";
+//Importing mongoose
+import mongoose from "mongoose";
+
+// import { MongoClient } from "mongodb";
 
 // Load environment variables from .env
 import dotenv from "dotenv";
 dotenv.config();
 
-const client = new MongoClient(process.env.ATLAS_URI);
+const gradesSchema = new mongoose.Schema({
+  // Schema fields
+});
 
-let conn;
-try {
-  conn = await client.connect();
-} catch (e) {
-  console.error(e);
+const Grades = mongoose.model('Grades', gradesSchema);
+
+async function connect() {
+  try {
+    await mongoose.connect(process.env.ATLAS_URI);
+    console.log('Connected to MongoDB');
+  } catch (err) {
+    console.error(err);
+  }
 }
 
-let db = conn.db("sample_training");
-const grades = db.collection("grades");
-// Single-field index on class_id
-grades.createIndex({ class_id: 1 });
+export default { Grades, connect };
 
-// Single-field index on learner_id
-grades.createIndex({ learner_id: 1 });
+// const client = new MongoClient(process.env.ATLAS_URI);
 
-// Compound index on class_id and learner_id
-grades.createIndex({ class_id: 1, learner_id: 1 });
-export default db;
+// let conn;
+// try {
+//   conn = await client.connect();
+// } catch (e) {
+//   console.error(e);
+// }
+
+// let db = conn.db("sample_training");
+
+// const grades = db.collection("grades");
+
+// grades.createIndex({ class_id: 1 });
+
+// grades.createIndex({ learner_id: 1 });
+
+// grades.createIndex({ class_id: 1, learner_id: 1 });
+
+// export default db;
--- db/mongoose.js
+++ db/mongoose.js
@@ -2,7 +2,7 @@
 import mongoose from 'mongoose';
 async function connect() {
   try {
-    await mongoose.connect('mongodb+srv://admin:admin@cluster0.cgnmk.mongodb.net/test');
+    await mongoose.connect('mongodb+srv://admin:admin@cluster0.cgnmk.mongodb.net/');
     console.log('Connected to MongoDB');
   } catch (err) {
     console.error(err);
